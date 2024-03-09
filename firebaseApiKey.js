45968793f59a593926dfbe4da59810765f1bf59c
SawaIto <158807816+SawaIto@users.noreply.github.com>
2024-03-09 12:05:04 +0900

    Delete firebaseApiKey.js

--- firebaseApiKey.js
+++
@@ -1,8 +0,0 @@
-    export default {
-        apiKey: "AIzaSyBRHWoYhTiZP8Zyp5ntB_Iivoae5ClP7_8",
-        authDomain: "sample-6f901.firebaseapp.com",
-        projectId: "sample-6f901",
-        storageBucket: "sample-6f901.appspot.com",
-        messagingSenderId: "282814902776",
-        appId: "1:282814902776:web:d1f0c4dac6a13687d1a60d"
-    };
